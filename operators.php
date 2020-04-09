0e7bc3a755a518f39af089f0c2754bac6fc32411
upieez <samuelhww@gmail.com>
2020-04-09 19:03:23 +0800

    php operators

---
+++ operators.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparison and Logical Operators</title>
+</head>
+<body>
+
+    <?php
+        $a = 4;
+        $b = 3;
+        $c = 20;
+        $d = 1;
+        if (($a > $b) && ($c > $d)){
+            echo "a is larger than b AND ";
+            echo "c is larger than d";
+        }
+    ?>
+    <br />
+    <?php
+        $a = 4;
+        $b = 3;
+        $c = 20;
+        $d = 1;
+        if (($a > $b) || ($c > $d)){
+            echo "a is larger than b OR ";
+            echo "c is larger than d";
+        }
+    ?>
+    <br />
+    <?php
+    $e = 100;
+        if (!isset($e)){
+            $e = 200;
+        }
+    echo $e;
+    ?>
+    <br />
+    <?php
+    // don't reject 0 or 0.0
+    $quantity = "";
+        if (empty($quantity) && !is_numeric($quantity)){
+            echo "You must enter a quantity";
+        }
+    ?>
+    
+</body>
+</html>
