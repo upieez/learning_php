e110dc01cd959d30acd68fb9273d3288b23514f4
upieez <samuelhww@gmail.com>
2020-04-09 18:21:10 +0800

    changing the type of the variable using type casting

---
+++ type_casting.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Type Juggling and Type Casting</title>
+</head>
+<body>
+
+    Type Juggling <br />
+    <?php $count = "2 cats"; ?>
+    Type: <?php echo gettype($count); ?><br />
+
+    <?php $count += 3; ?>
+    Type: <?php echo gettype($count); ?><br />
+
+    <?php $cats = "I have " . $count . " cats."; ?>
+    Cats: <?php echo gettype($cats); ?><br />
+    <br />
+
+    Type Casting<br />
+    <?php settype($count, "integer"); ?>
+    count: <?php echo gettype($count); ?><br />
+    <br />
+
+    <?php $count2 = (string) $count; ?>
+    count: <?php echo gettype($count); ?><br />
+    count2: <?php echo gettype($count2); ?><br />
+    <br />
+
+    <?php $test1 = 3; ?>
+    <?php $test2 = 3; ?>
+    <?php settype($test1, "string"); ?>
+    <?php (string) $test2; //This will only happen when it is being used at the moment ?>
+    test1: <?php echo gettype($test1); ?><br />
+    test2: <?php echo gettype($test2); ?><br />
+    
+</body>
+</html>
