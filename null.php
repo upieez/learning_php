74f1e055b80cc4b9094efec3104e583f31d63140
upieez <samuelhww@gmail.com>
2020-04-09 18:11:44 +0800

    php null

---
+++ null.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NULL</title>
+</head>
+<body>
+
+    <?php
+        $var1 = null;
+        $var2 = "";
+    ?>
+
+    var1 null? <?php echo is_null($var1); ?><br />
+    var2 null? <?php echo is_null($var2); ?><br />
+    var3 null? <?php echo is_null($var3); // Will return true even if there is not variable ?><br />
+    <br />
+    var1 is set? <?php echo isset($var1); ?><br />
+    var2 is set? <?php echo isset($var2); ?><br />
+    var3 is set? <?php echo isset($var3); ?><br />
+    <br />
+    <?php // empty: "", null, 0, 0.0, "0", false, array() ?>
+
+    <?php $var3 = "0"; ?>
+    var3 is empty? <?php echo empty($var3); ?><br />
+    var3 is empty? <?php echo empty($var3); ?><br />
+    var3 is empty? <?php echo empty($var3); ?><br />
+
+
+</body>
+</html>
