f3c0365f133a38bff51332693791710f7286910c
upieez <samuelhww@gmail.com>
2020-04-09 18:51:33 +0800

    if and elseif statements for php

---
+++ logical.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logical</title>
+</head>
+<body>
+    
+    <?php 
+    $a = 4;
+    $b = 4;
+        if ($a > $b) {
+            echo "a is larger than b";
+        } elseif ($a < $b) {
+            echo "a is smaller than b";
+        } else {
+            echo "a is equal to b";
+        }
+    ?>
+    <br />
+    <?php // Only welcome new users
+    $new_user = true;
+        if ($new_user) {
+            echo "<h1>Welcome!</h1>";
+            echo "<p>We are glad you decided to join us.</p>";
+        }
+    ?>
+    <br />
+    <?php // do not divide by zero
+    $numerator = 20;
+    $denominator = 4;
+    $result = 0;
+        if ($denominator > 0) {
+            $result = $numerator / $denominator;
+        }
+        echo "Result: {$result}";
+    ?>
+
+</body>
+</html>
